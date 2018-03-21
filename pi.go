757e4bbda567445ccf1b39bc1470e821e25ff120
Remus Knowles <remknowles@gmail.com>
2018-03-21 11:42:16 +0000

    Clean up with goimports.

--- pi.go
+++ pi.go
@@ -20,19 +20,19 @@ func main() {
 	res := make(chan float64)
 	chanCnt := make(chan int)
 
-	for i:=0;i<4;i++ {
+	for i := 0; i < 4; i++ {
 		go func() {
 			lcnt := 0
 			lin := 0
 			for {
 				lcnt += 1
-				x, y := (2 * rand.Float64()) - 1, (2 *rand.Float64()) - 1
+				x, y := (2*rand.Float64())-1, (2*rand.Float64())-1
 
-				if x*x + y*y < 1 {
+				if x*x+y*y < 1 {
 					lin += 1
 				}
 
-				if lcnt % printOn == 0 {
+				if lcnt%printOn == 0 {
 					mutex.Lock()
 					cnt += lcnt
 					in += lin
@@ -45,6 +45,6 @@ func main() {
 	}
 
 	for {
-		fmt.Println(<- res, <- chanCnt)
+		fmt.Println(<-res, <-chanCnt)
 	}
 }
